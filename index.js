933a3bed29f7e4a0ef1969c37808c852603af939
minibox24 <minibox724@gmail.com>
2021-03-18 14:12:39 +0900

    feat: nav btn

--- index.js
+++ index.js
@@ -1,4 +1,48 @@
+function elementInViewport(el) {
+    let top = el.offsetTop;
+    let left = el.offsetLeft;
+    const width = el.offsetWidth;
+    const height = el.offsetHeight;
+
+    while (el.offsetParent) {
+        el = el.offsetParent;
+        top += el.offsetTop;
+        left += el.offsetLeft;
+    }
+
+    return (
+        top < (window.pageYOffset + window.innerHeight) &&
+        left < (window.pageXOffset + window.innerWidth) &&
+        (top + height) > window.pageYOffset &&
+        (left + width) > window.pageXOffset
+    );
+}
+
 const movePage = (name) => {
     const el = document.getElementById(name)
+    if (!el) return
     el.scrollIntoView({ behavior: 'smooth' })
+}
+
+const getViewId = () => {
+    const pages = Array.from(document.querySelectorAll('.page'))
+    const pageId = pages.map(el => el.id)
+    let views = []
+    pages.forEach(el => {
+        if (elementInViewport(el)) views.push(el.id)
+    })
+
+    return pageId.indexOf(views[views.length - 1])
+}
+
+const up = () => {
+    const pages = Array.from(document.querySelectorAll('.page'))
+    const pageId = pages.map(el => el.id)
+    movePage(pageId[getViewId() - 1])
+}
+
+const down = () => {
+    const pages = Array.from(document.querySelectorAll('.page'))
+    const pageId = pages.map(el => el.id)
+    movePage(pageId[getViewId() + 1])
 }
